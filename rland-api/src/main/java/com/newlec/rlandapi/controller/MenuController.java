8d76138656831b5fb89e811b2568548288921809
hi0424 <tkdals9903@naver.com>
2023-01-13 12:04:44 +0900

    make MenuController

--- rland-api/src/main/java/com/newlec/rlandapi/controller/MenuController.java
+++ rland-api/src/main/java/com/newlec/rlandapi/controller/MenuController.java
@@ -1,13 +1,35 @@
 package com.newlec.rlandapi.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("menu")
+@RequestMapping("menus")
 public class MenuController {
+
+    @GetMapping // 더 추가적인 url없음
+    public String getMenu(){
+        return "get Menu";
+    }
+
+    @PostMapping
+    public String addMenu(){
+        return "add Menu";
+    }
+
+    @PutMapping
+    public String updateMenu(){
+        return "update Menu";
+    }
+
+    @DeleteMapping
+    public String deleteMenu(){
+        return "delete Menu";
+    }
 
     @GetMapping("hello")
     public String hello(){
