58ae0d44b826ef75485d035f6a9c465c40afbc36
hi0424 <tkdals9903@naver.com>
2023-01-13 11:48:40 +0900

    first commit

---
+++ rland-api/src/test/java/com/newlec/rlandapi/RlandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newlec.rlandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
