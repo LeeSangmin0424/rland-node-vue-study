302223fb9db5252974119bce12eda3d114745f41
hi0424 <tkdals9903@naver.com>
2023-01-13 20:18:53 +0900

    vsCode에서 이클립스처럼 java을 사용한다. Postman을 이용해서 서버를 사용하지 않고 API를 요청한다.

--- rland-api/src/main/java/com/newlec/rlandapi/controller/MenuController.java
+++ rland-api/src/main/java/com/newlec/rlandapi/controller/MenuController.java
@@ -5,29 +5,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("menus")
 public class MenuController {
+    // page풀네임이 필요. p와 page / url은 p, 
+    // /menus?p=1&s=15
+    // /menus만 쓰면 null이 나온다. -> default값을 줘야 한다. 
+    @GetMapping("") // 더 추가적인 url없음
+    public String getList(
+        @RequestParam(name ="p", defaultValue = "1") int page, 
+        @RequestParam(name ="s", defaultValue = "15") int size){
 
-    @GetMapping // 더 추가적인 url없음
-    public String getMenu(){
-        return "get Menu";
+        return String.format("getMenu List:page=%d, size%d",page,size);
+    }
+
+    // menus/2
+    @GetMapping("{id}")
+    public String get(int id){
+        return String.format("get Menu: id=%id,id");
     }
 
     @PostMapping
-    public String addMenu(){
+    public String add(){
         return "add Menu";
     }
 
     @PutMapping
-    public String updateMenu(){
+    public String update(){
         return "update Menu";
     }
 
     @DeleteMapping
-    public String deleteMenu(){
+    public String delete(){
         return "delete Menu";
     }
 
