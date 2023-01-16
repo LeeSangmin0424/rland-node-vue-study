2976ad5e8bc712477d4f257e7a66e4beab0577e4
hi0424 <tkdals9903@naver.com>
2023-01-16 23:07:43 +0900

    1. lombok 사용 2. 출력을 json, xml로 하기 위한 build.gradle 수작업으로 추가. postman 헤더 설정 3. ResponseEntity 응답 전용 객체 - 내가 지정할 수 있다. . RequestBody 5.Bean  Valid 설정 6. 예외처리4

--- rland-api/src/main/java/com/newlec/rlandapi/controller/MenuController.java
+++ rland-api/src/main/java/com/newlec/rlandapi/controller/MenuController.java
@@ -1,12 +1,21 @@
 package com.newlec.rlandapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.newlec.rlandapi.entity.Menu;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("menus")
@@ -23,24 +32,58 @@ public class MenuController {
     }
 
     // menus/2
-    @GetMapping("{id}")
-    public String get(int id){
-        return String.format("get Menu: id=%id,id");
+    // @GetMapping(path = "{id}", produces = {
+
+    //      MediaType.APPLICATION_JSON_VALUE,
+    //      MediaType.APPLICATION_XML_VALUE
+
+    //     }) // 문자열을 배열로 받는다. 
+    // // public Menu get(@PathVariable int id){
+    //     public ResponseEntity<Menu> get(@PathVariable int id){ // 내가 조작한 오류
+
+
+
+    //     Menu menu = Menu 
+    //                     .builder()
+    //                     .name( "아이스 카페라떼")
+    //                     .price( 3000)
+    //                     .build();
+
+    //     // System.out.println(menu);
+
+    //     // return String.format("get Menu: id= %d",id);
+    //     // return menu;
+    //     return new ResponseEntity<Menu>(HttpStatus.BAD_REQUEST);
+    // }
+
+    @PostMapping(produces = {},consumes = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_XML_VALUE
+    }) // 돌려주는 것이기 때문에 produces설정 가능 // consumes : 어떤 포맷으로 받을 까?
+    public Menu create( @RequestBody Menu menu){ // 데이터를 받아야 한다. // 현재 만든 menu를 반환 //@RequestBody=> xml로 받음.
+        
+        System.out.println(menu);
+
+        // db에 저장
+        // 방금 저장한 데이터를 다시 가져와서
+        return menu; 
     }
 
-    @PostMapping
-    public String add(){
-        return "add Menu";
+
+    @PutMapping // json이 기본이라 consume을 설정하지 않음.
+    public Menu update(@RequestBody Menu menu){ // menu를 통으로 받는다.
+        Menu a = null;
+        a.getId();
+
+        return menu;
+
     }
 
-    @PutMapping
-    public String update(){
-        return "update Menu";
-    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@RequestBody int id){
 
-    @DeleteMapping
-    public String delete(){
-        return "delete Menu";
+        // 삭제
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("hello")
